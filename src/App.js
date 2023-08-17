99775d29001a2d8756fa6608fd39139f05e5655f
Jangwoo Park <p.jangwoo@gmail.com>
2023-08-17 17:27:14 +0900

    8월 17일 - 오후 5시

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+//JSON 파일을 import
+import {dummy} from "./movieDummy"; 
 
+//Movie 컴포넌트 import
+import Movie from "./component/Movie";
+
+// JSON 파일의 필드를 읽어서 Movie 컴포넌트의 매개변수로 주입 
 function App() {
   return (
     <div>
-        <h1> 초기 셋팅 완료 </h1>
+        <div className = "app-container">
+          {
+            dummy.results.map( (item) => {
+              return <Movie 
+                    title = {item.title}
+                    poster_path={item.poster_path}
+                    vote_average={item.vote_average}
+              />
+            }
+            )
+          }
+        </div>
     </div>
   );
 }
